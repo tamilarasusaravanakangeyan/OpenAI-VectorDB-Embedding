b80fb75c13884543892d1dfc77c214ecc6df4529
TamilarasuSaravanakangeyan <tamilsmtp@gmail.com>
2024-05-24 02:28:04 +0300

    Open AI text embedding implementation

---
+++ index.js
@@ -0,0 +1,27 @@
+const https = require('https');
+const _api_Key = '<OpenAI API Key>';
+const data = JSON.stringify({
+    "input": "A long.",
+    "model": "text-embedding-ada-002",
+    "encoding_format":"float"
+});
+const options = {
+    hostname: 'api.openai.com',
+    port: 443,
+    path: '/v1/embeddings',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + _api_Key,
+    }
+};
+
+const req = https.request(options, (res) => {
+    console.log('statusCode:', res.statusCode);
+    console.log('headers:', res.headers);
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
+});
+req.write(data);
+req.end();
